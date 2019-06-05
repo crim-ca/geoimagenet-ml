c3adf08c623c3fb3da3f8df3cda93d8144a2b3ab
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2019-06-05 18:11:30 -0400

    sentry server name config var

--- geoimagenet_ml/api/main.py
+++ geoimagenet_ml/api/main.py
@@ -9,16 +9,19 @@ from pyramid.config import Configurator
 from pyramid.security import NO_PERMISSION_REQUIRED
 from pyramid.settings import asbool
 import logging
+import socket
 import os
 LOGGER = logging.getLogger(__name__)
 
 sentry_dsn = os.getenv("GEOIMAGENET_ML_SENTRY_DSN")
 sentry_debug = asbool(os.getenv("GEOIMAGENET_ML_SENTRY_DEBUG", False))
+sentry_server_name = os.getenv("GEOIMAGENET_ML_SENTRY_SERVER_NAME", socket.gethostname())
 if sentry_dsn:
     import sentry_sdk
     from sentry_sdk.integrations.celery import CeleryIntegration
     from sentry_sdk.integrations.pyramid import PyramidIntegration
-    sentry_sdk.init(sentry_dsn, debug=sentry_debug, integrations=[CeleryIntegration(), PyramidIntegration()])
+    sentry_sdk.init(sentry_dsn, debug=sentry_debug, server_name=sentry_server_name,
+                    integrations=[CeleryIntegration(), PyramidIntegration()])
 
 
 # noinspection PyUnusedLocal
