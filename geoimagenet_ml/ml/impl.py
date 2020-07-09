d1299257c1b922bb45de2286b5021021ac35a769
Samuel Foucher <samuel.foucher@crim.ca>
2020-07-08 23:02:52 -0400

    Modifications to handle Segmentation tasks

--- geoimagenet_ml/ml/impl.py
+++ geoimagenet_ml/ml/impl.py
@@ -389,15 +389,17 @@ def test_loader_from_configs(model_checkpoint_config, model_config_override, dat
                      f"  original task: [{task_str}]\n"
                      f"  modified task: [{task_params}]")
         test_config["task"] = {"params" : task_params, "type" : task_class_str}
-    # task_type = thelper.utils.import_class(test_config["task"]["type"])
-    # if task_class_str == "thelper.tasks.detect.Detection":
-    #    test_config["task"]["params"].update({"input_key": IMAGE_DATA_KEY})
-    # else:
-    if "Detection" not in task_class_str:
-        test_config["task"]["params"].update({"input_key": IMAGE_DATA_KEY, "label_key": IMAGE_LABEL_KEY})
+        if "Detection" in task_class_str:
+            test_config["task"]["params"].update({"input_key": IMAGE_DATA_KEY})
+        elif "Segmentation" in task_class_str:
+            test_config["task"]["params"].update({"input_key": IMAGE_DATA_KEY, "label_map_key": IMAGE_LABEL_KEY})
+        else:
+            test_config["task"]["params"].update({"input_key": IMAGE_DATA_KEY, "label_key": IMAGE_LABEL_KEY})
+        test_model_task = thelper.tasks.create_task(test_config["task"])
     else:
-        test_config["task"]["params"].update({"input_key": IMAGE_DATA_KEY})
-    test_model_task = thelper.tasks.create_task(test_config["task"])
+        task_class_str = fully_qualified_name(test_config["task"])
+        test_model_task = test_config["task"]
+
     test_model_task_name = fully_qualified_name(test_model_task)
     test_config["config"]["name"] = model_config_override["name"]
     test_config["config"]["datasets"] = {
@@ -849,5 +851,11 @@ MODEL_TASK_MAPPING = {
         MAPPING_LOADER: fully_qualified_name(BatchTestPatchesClassificationDatasetLoader),
         MAPPING_TESTER: fully_qualified_name(thelper.train.classif.ImageClassifTrainer),
         MAPPING_RESULT: classification_test_results_finder,
-    }
+    },
+    fully_qualified_name(thelper.tasks.segm.Segmentation): {
+        MAPPING_TASK:   fully_qualified_name(thelper.tasks.segm.Segmentation),
+        MAPPING_LOADER: fully_qualified_name(BatchTestPatchesClassificationDatasetLoader), #FIXME: should point to a segmentation loader
+        MAPPING_TESTER: fully_qualified_name(thelper.train.segm.ImageSegmTrainer),
+        MAPPING_RESULT: classification_test_results_finder, #FIXME: should point to a segmentation evaluation
+    },
 }
